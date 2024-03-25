de74a7b487ac23ce04811fee0eeb791adcc7f315
boyuchen2022 <119028581+boyuchen2022@users.noreply.github.com>
2024-03-25 19:25:41 +0800

    Add files via upload

---
+++ user-center-backend-master/src/test/java/com/yupi/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,29 @@
+package com.yupi.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 启动类测试
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class UserCenterApplicationTests {
+
+
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
